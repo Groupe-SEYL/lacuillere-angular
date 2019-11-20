4cefec7e3e863a9c14ae1a4577ceaea17ac56124
Shadi Boomi <shadi.boomi@gmail.com>
2019-11-20 14:46:12 +0100

    Changed title for view

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'lacuillere-angular'`, () => {
+  it(`should have as title 'La Cuillère'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('lacuillere-angular');
+    expect(app.title).toEqual('La Cuillère');
   });
 
   it('should render title', () => {
