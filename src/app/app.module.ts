269d9ff70e0658553f8d7c22d2afa8659e0e8fbf
Shadi Boomi <shadi.boomi@gmail.com>
2019-11-20 16:04:17 +0100

    Added: component restaurant component menu restaurant.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaurantComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
