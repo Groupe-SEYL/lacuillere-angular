b243c8a271cdcf2295d2039ea08d14acabf1c1c9
Shadi Boomi <shadi.boomi@gmail.com>
2019-11-21 16:16:15 +0100

    Edited restaurant-form component

--- src/app/restaurant-form/restaurant-form.component.ts
+++ src/app/restaurant-form/restaurant-form.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RestaurantService } from '../service/restaurant.service';
+import { Restaurant } from '../model/restaurant';
 
 @Component({
   selector: 'app-restaurant-form',
   templateUrl: './restaurant-form.component.html',
   styleUrls: ['./restaurant-form.component.css']
 })
-export class RestaurantFormComponent implements OnInit {
 
-  constructor() { }
+export class RestaurantFormComponent {
+	restaurant: Restaurant;
 
-  ngOnInit() {
+  constructor(
+  	private route: ActivatedRoute,
+  	private router: Router,
+  	private restaurantService: RestaurantService) { 
+  	this.restaurant = new Restaurant();
   }
+
+  onSubmit() {
+  	this.restaurantService.save(this.restaurant).subscribe(result => this.gotoRestaurantList());
+  }
+
+  gotoRestaurantList(){
+  	this.router.navigate(['/restaurants']);
+  }
+
+  
 
 }
