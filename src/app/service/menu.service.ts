9682c4ccc4f615d280fe11e89d5cc622639e7f39
Shadi Boomi <shadi.boomi@gmail.com>
2019-11-21 15:13:29 +0100

    Edited menu.service and added a connection with the /menu mapping

--- src/app/service/menu.service.ts
+++ src/app/service/menu.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders } from '@angular/common/http';
+import { Menu } from '../model/menu';
+import { Observable } from 'rxjs/Observable';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class MenuServiceService {
+@Injectable()
+export class MenuService {
+	private menusUrl: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  	this.menusUrl = 'http://localhost:8080/menu';
+   }
+
+   public findAll(): Observable<Menu[]> {
+   		return this.http.get<Menu[]>(this.menusUrl);
+   }
+
+   public save(menu: Menu){
+   	return this.http.post<Menu>(this.menusUrl, menu);
+   }
 }
