2ec9ffa4909e68747e7c1ccb3267d08d64e82f1a
Shadi Boomi <shadi.boomi@gmail.com>
2019-11-21 16:08:31 +0100

    Modified restaurant service to include GET POST and DELETE methods

--- src/app/service/restaurant.service.ts
+++ src/app/service/restaurant.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Restaurant } from '../model/restaurant';
+import { Observable } from 'rxjs/Observable';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class RestaurantService {
+  private restaurantsUrl: string;
+  private newRestaurantURL: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+  		this.restaurantsUrl = 'http://localhost:8080/restaurants';
+  		this.newRestaurantURL = 'http://localhost:8080/newrestaurant';
+  }
+
+  public findAll(): Observable<Restaurant[]> {
+  	return this.http.get<Restaurant[]>(this.restaurantsUrl);
+  }
+
+  public save(restaurant: Restaurant) {
+  	return this.http.post<Restaurant>(this.newRestaurantURL, restaurant);
+  }
+
+
+  /** DELETE: delete the restaurant from the server */
+ public delete(id: number): Observable<{}> {
+  const url = '${this.restaurantsUrl}/${id}'; // DELETE api/heroes/42
+  return this.http.delete(url);
+}
+
+
 }
